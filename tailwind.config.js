7f19af36f617933ce4a6ce299466a5fd5aa2fea5
Kevin <54258753+vCiKv@users.noreply.github.com>
2023-03-18 10:19:28 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "dark-grayish": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)",
+      },
+    },
+  },
+  plugins: [],
+};
